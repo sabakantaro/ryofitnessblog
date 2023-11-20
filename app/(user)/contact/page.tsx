e9b6920f7b020d05cefd7620bccb7d295e405ba9
sabakantaro <sbkryo290112@yahoo.co.jp>
2023-11-19 20:52:54 -0800

    Fix placeholders on the contact page

--- app/(user)/contact/page.tsx
+++ app/(user)/contact/page.tsx
@@ -18,7 +18,7 @@ export default async function HomePage() {
               type='email'
               id='email'
               className='shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
-              placeholder='name@flowbite.com'
+              placeholder='ryo@fitness.com'
               required
             />
           </div>
@@ -30,7 +30,7 @@ export default async function HomePage() {
               type='text'
               id='subject'
               className='block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light'
-              placeholder='Let us know how we can help you'
+              placeholder='Please enter a subject...'
               required
             />
           </div>
@@ -41,7 +41,7 @@ export default async function HomePage() {
             <textarea
               id='message'
               className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-              placeholder='Leave a comment...'
+              placeholder='Please leave a comment...'
             ></textarea>
           </div>
           <button
